f69685522126648ee15e0141abb098425275c559
Emil Ohlsson <emil@kottland.net>
2018-01-04 12:20:13 +0100

    Solved part 8

---
+++ src/bin/08-product.rs
@@ -0,0 +1,16 @@
+#[macro_use]
+extern crate project_euler;
+
+fn main() {
+    let input = include_str!("input-08");
+    let digs = input
+        .lines()
+        .flat_map(|s| s.chars())
+        .map(|c| c.to_digit(10).unwrap() as usize)
+        .collect::<Vec<usize>>();
+    let ans = digs.windows(13)
+        .map(|w| w.iter().fold(1, |a, v| a * v))
+        .max()
+        .unwrap();
+    answer!("{}", ans);
+}
