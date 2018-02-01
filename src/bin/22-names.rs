ab18028c30f6eb84b298f0ab501833288318ed1f
Emil Ohlsson <emil@kottland.net>
2018-02-01 12:46:53 +0100

    Solve 22

---
+++ src/bin/22-names.rs
@@ -0,0 +1,21 @@
+#[macro_use]
+extern crate project_euler;
+
+fn main() {
+    let mut names = include_str!("input-22")
+        .split(",")
+        .map(|s| s.trim_matches('"'))
+        .collect::<Vec<&str>>();
+    names.sort();
+    let sum = names
+        .iter()
+        .enumerate()
+        .map(|(i, w)| {
+            let wsum = w.as_bytes().iter().map(|c| c - 64u8).sum::<u8>() as usize;
+            let res = (i + 1) * wsum;
+            println!("{}: {} * {} = {}", w, (i + 1), wsum, res);
+            res
+        })
+        .sum::<usize>();
+    answer!("{}", sum);
+}
