66f72d53f039199e2e0a968a5b5cfc16586bb04b
Emil Ohlsson <emil@kottland.net>
2018-02-07 23:07:37 +0100

    Solve 26

---
+++ src/bin/26-reciprocal.rs
@@ -0,0 +1,34 @@
+#[macro_use]
+extern crate project_euler;
+
+use std::collections::HashMap;
+
+fn reciprocal_length(div: usize) -> usize {
+    let mut states: HashMap<(usize, usize), usize> = HashMap::new();
+    let mut nom: usize = 1;
+
+    for i in 0.. {
+        if nom == 0 {
+            break;
+        }
+        while nom / div == 0 {
+            nom *= 10;
+        }
+        let pair = (nom, div);
+
+        if let Some(&prev) = states.get(&pair) {
+            return i - prev;
+        }
+        states.insert(pair, i);
+        nom = nom - div * (nom / div);
+    }
+
+    0
+}
+
+fn main() {
+    answer!(
+        "{}",
+        (1..1001).max_by_key(|&i| reciprocal_length(i)).unwrap()
+    );
+}
