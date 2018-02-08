7510cf32527a17904a9c98c71ef78d02921ba093
Emil Ohlsson <emil@kottland.net>
2018-02-08 22:32:36 +0100

    Solve 27

---
+++ src/bin/27-quadratic.rs
@@ -0,0 +1,37 @@
+#[macro_use]
+extern crate project_euler;
+
+use std::collections::HashSet;
+use project_euler::series::Primes;
+
+fn main() {
+    let mut a_best = 0;
+    let mut b_best = 0;
+    let mut best = 0;
+
+    let primes: HashSet<isize> = {
+        let mut p = Primes::new();
+        p.take(1000)
+            .map(|n: usize| n as isize)
+            .collect::<HashSet<isize>>()
+    };
+
+    for a in -1000..1001 {
+        for b in -1000..1001 {
+            for n in 0.. {
+                let pc = n * n + a * n + b;
+                if !primes.contains(&pc) {
+                    if n > best {
+                        println!("Best is {}", best);
+                        a_best = a;
+                        b_best = b;
+                        best = n;
+                    }
+                    break;
+                }
+            }
+        }
+    }
+
+    answer!("{}", a_best * b_best);
+}
