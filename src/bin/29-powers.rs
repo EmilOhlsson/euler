527f0a630cd1537db10c57efd18b9b7734c1e2dd
Emil Ohlsson <emil@kottland.net>
2018-02-19 07:43:50 +0100

    solve 29

---
+++ src/bin/29-powers.rs
@@ -0,0 +1,24 @@
+extern crate num;
+#[macro_use]
+extern crate project_euler;
+
+use num::bigint::BigUint;
+use std::collections::HashSet;
+
+fn bigpow(a: usize, b: usize) -> BigUint {
+    let mut res = BigUint::from(a);
+    for _ in 1..b {
+        res *= a;
+    }
+    return res;
+}
+
+fn main() {
+    let mut set = HashSet::new();
+    for a in 2..101 {
+        for b in 2..101 {
+            set.insert(bigpow(a, b));
+        }
+    }
+    answer!("{}", set.len());
+}
