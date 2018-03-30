f1122dcf95de7419f667203e0b9116c59c229699
Emil Ohlsson <kotte@kottland.net>
2018-03-30 22:02:34 +0200

    Solve problem 31

---
+++ src/bin/31-coins.rs
@@ -0,0 +1,20 @@
+#[macro_use]
+extern crate project_euler;
+
+fn rec(coins: &Vec<usize>, i: usize, acc: usize, target: usize) -> usize {
+    if let Some(coin) = coins.get(i) {
+        if acc == target {
+            1
+        } else if acc > target {
+            0
+        } else {
+            rec(coins, i, acc + coin, target) + rec(coins, i + 1, acc, target)
+        }
+    } else {
+        0
+    }
+}
+
+fn main() {
+    answer!("{}", rec(&vec![200, 100, 50, 20, 10, 5, 2, 1], 0, 0, 200));
+}
